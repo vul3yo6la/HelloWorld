4070be70338cc8a0ffece0b107411671409a7e7e
Ian <vul3yo6la@gmail.com>
2025-01-26 00:32:22 +0800

    test_branch

--- SnackIt/Form1.cs
+++ SnackIt/Form1.cs
@@ -37,6 +37,7 @@ namespace SnackIt
         public Random _r;
         public int _dir = (int)Direction.RIGHT;
         public int _ScoreCount = 1;
+        public int test_branch = 10;
         Thread SnackM;
 
         Color[,] bgColors = new Color[20, 20];
